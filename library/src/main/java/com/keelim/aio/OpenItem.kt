7c95b2e3cd0def5b9edc7f4a98d84ed06c5586d8
h0033 <kimh00335@gmail.com>
2020-07-12 23:14:43 +0900

    2020 07 12

---
+++ library/src/main/java/com/keelim/aio/OpenItem.kt
@@ -0,0 +1,19 @@
+package com.keelim.aio
+
+class OpenItem {
+    constructor(title: String?, desc: String?) {
+        this.title = title
+        this.desc = desc
+
+    }
+
+    constructor() {
+        title = null
+        desc = null
+
+    }
+
+    var title: String? = null
+    var desc: String? = null
+
+}
